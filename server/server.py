5924d4210ddc4af889329f47861cda9bb988e93c
ardra1210 <ardra12kottodam@gmail.com.com>
2026-05-23 13:20:23 +0530

    server fix

--- server/server.py
+++ server/server.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin','*')
-
     return response
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
@@ -20,9 +20,6 @@ def predict_home_price():
     response = jsonify({
         'estimated_price': util.get_estimated_price(location, total_sqft,  bhk, bath)
     })
-
-    response.headers.add('Access-Control-Allow-Origin','*')
-
     return response
 
 if __name__ == "__main__":
